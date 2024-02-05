9c1b9947a2670de65810e897380071b4453e758c
decotart <dfyrkin@bk.ru>
2024-02-05 22:02:33 +0300

    Добавьте файлы проекта.

---
+++ Praktika/Class1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace Praktika
+{
+    public class Ex1
+    {
+
+        /// <summary>
+        /// Определяет является ли сумма цифр трехзначного числа двухзначным числом 
+        /// </summary>
+        /// <param name="value">трехзначное число</param>
+        /// <returns>True если сумма цифр - двухзначное число, false - если иначе</returns>
+        /// <exception cref="ArgumentException">Число не является трехзначным </exception>
+        public static bool IsSummOfDigitsDoubleDigit(int value)
+        {
+            if (value <= 999 && value >= 100)
+            {
+                int summ = (value / 100) + (value / 10 % 10) + (value % 10);
+                
+                if (summ <= 99 && summ >= 10)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else
+            {
+                throw new ArgumentException("Число не является трехзначным");
+            }
+        }
+    }
+
+    public class Ex2
+    {
+
+        /// <summary>
+        /// найти сумму двух наибольших чисел
+        /// </summary>
+        /// <param name="value1">Первое число</param>
+        /// <param name="value2">Второе число</param>
+        /// <param name="value3">Третье число</param>
+        /// <returns>Сумма двух наибольших из чисел</returns>
+        public static int GetSummOfTwoMaxValues(int value1, int value2, int value3)
+        {
+            var list = new List<int>() { value1, value2, value3 };
+            
+            return list.OrderByDescending(x => x).Take(2).Sum();
+        }
+    }
+    
+    public class Ex3
+    {
+        /// <summary>
+        /// Находит элемент, наиболее близкий к среднему значению всех элементов массива
+        /// </summary>
+        /// <param name="mas">двухзначный массив</param>
+        /// <returns>Наиболее близкий элемент к среднему значению всех элементов массива</returns>
+        public static double GetNearestToAverage(int[,] mas)
+        {
+            double average = GetAverage(mas),
+                nearestValue = mas[0,0];
+
+            foreach(int i in mas)
+            {
+                if (Math.Abs(i - average) < Math.Abs(nearestValue - average))
+                {
+                    nearestValue = i;
+                }
+            }
+
+            return nearestValue;
+        }
+
+
+        /// <summary>
+        /// Находит среднее значение элементов массива
+        /// </summary>
+        /// <param name="mas">Двухзначный массив</param>
+        /// <returns>Среднее значение всех элементов</returns>
+        public static double GetAverage(int[,] mas)
+        {
+            int sum = 0;
+
+            foreach (int i in mas)
+            {
+                sum += i;
+            }
+
+            return sum / mas.Length;
+        }
+    }
+
+    public class Ex4
+    {
+        /// <summary>
+        /// Формирует одноменрный массив из количества положительных элементов столбцов матрицы
+        /// </summary>
+        /// <param name="matrix">Матрица</param>
+        /// <returns>Одноменрный массив из количества положительных элементов столбцов матрицы</returns>
+        public static int[] CreateArray(int[,] matrix)
+        {
+            int[] mas = new int[matrix.GetLength(1)];
+            int count;
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                count = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (matrix[i, j] >= 0)
+                    {
+                        count++;
+                    }
+                }
+                mas[j] = count;
+            }
+            return mas;
+        }
+    }
+
+    public static class VisualArray
+    {
+        public static DataTable ToDataTable<T>(this T[] arr)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+            var row = res.NewRow();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row[i] = arr[i];
+            }
+            res.Rows.Add(row);
+            return res;
+        }
+        public static DataTable ToDataTable<T>(this T[,] matrix)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var row = res.NewRow();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+
+                res.Rows.Add(row);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Praktika/MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Praktika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetResultEx1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int value = Convert.ToInt32(tbValueEx1.Text);
+
+                if (Ex1.IsSummOfDigitsDoubleDigit(value))
+                {
+                    MessageBox.Show("Сумма цифр является двухзначным числом");
+                }
+                else MessageBox.Show("Сумма цифр не является двухзначным числом");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так!");
+            }
+        }
+
+        private void btnGetResult2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int value1 = Convert.ToInt32(tbEx2Value1.Text),
+                    value2 = Convert.ToInt32(tbEx2Value2.Text),
+                    value3 = Convert.ToInt32(tbEx2Value3.Text);
+
+                int result = Ex2.GetSummOfTwoMaxValues(value1, value2, value3);
+
+                MessageBox.Show($"Сумма двух наибольших чисел равна {result}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+
+        }
+
+        int[,] matrix3;
+
+        private void btnCreateEx3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(tbNEx3.Text),
+                m = Convert.ToInt32(tbMEx3.Text);
+                matrix3 = new int[m, n];
+
+                DataGridEx3.ItemsSource = VisualArray.ToDataTable(matrix3).DefaultView;
+            }
+            catch (FormatException) 
+            { 
+                MessageBox.Show("Вводите только целочисленные значения!"); 
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnFillEx3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Random rnd = new();
+
+                for (int i = 0; i < matrix3.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix3.GetLength(1); j++)
+                    {
+                        matrix3[i, j] = rnd.Next(0, 10);
+                    }
+                }
+
+                DataGridEx3.ItemsSource = VisualArray.ToDataTable(matrix3).DefaultView;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Для начала создайте таблицу!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnGetResultEx3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double result = Ex3.GetNearestToAverage(matrix3);
+                
+                tbResultEx3.Text = result.ToString();
+
+                MessageBox.Show($"Среднее значение: {Ex3.GetAverage(matrix3)}");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void DataGridEx3_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                int index0 = e.Column.DisplayIndex;
+                int index1 = e.Row.GetIndex();
+
+                int value = int.Parse(((TextBox)e.EditingElement).Text);
+                matrix3[index1, index0] = value;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+
+            tbResultEx3.Clear();
+        }
+
+
+        //ex4
+
+        int[,] matrix4;
+        int[] result;
+
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                int index0 = e.Column.DisplayIndex;
+                int index1 = e.Row.GetIndex();
+
+                int value = int.Parse(((TextBox)e.EditingElement).Text);
+                matrix4[index1, index0] = value;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnGetResultEx4_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result = Ex4.CreateArray(matrix4);
+
+                DataGridResultEx4.ItemsSource = VisualArray.ToDataTable(result).DefaultView;
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnFillEx4_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Random rnd = new();
+
+                for (int i = 0; i < matrix4.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix4.GetLength(1); j++)
+                    {
+                        matrix4[i, j] = rnd.Next(-10, 10);
+                    }
+                }
+
+                DataGridEx4.ItemsSource = VisualArray.ToDataTable(matrix4).DefaultView;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Для начала создайте таблицу!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+        }
+
+        private void btnCreateEx4_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(tbNEx4.Text),
+                m = Convert.ToInt32(tbMEx4.Text);
+                matrix4 = new int[m, n];
+
+                DataGridEx4.ItemsSource = VisualArray.ToDataTable(matrix4).DefaultView;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только целочисленные значения!");
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так");
+
+            }
+        }
+    }
+}
